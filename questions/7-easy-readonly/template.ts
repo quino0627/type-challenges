265962d31ffc9b2d2f8524398f79bab7353f9ded
Mong <mong@channel.io>
2021-04-11 19:59:01 +0900

    7번문제 - ReadOnly 정답 및 샘플 작성

--- questions/7-easy-readonly/template.ts
+++ questions/7-easy-readonly/template.ts
@@ -1,1 +1,18 @@
-type MyReadonly<T> = any
+type MyReadonly<T> = {
+  readonly [t in keyof T]: T[t]
+}
+
+interface Todo {
+  title: string
+  description: string
+}
+
+const todo: MyReadonly<Todo> = {
+  title: "Hey",
+  description: "foobar",
+}
+
+// @ts-ignore
+todo.title = "Hello"
+// @ts-ignore
+todo.description = "barFoo"
