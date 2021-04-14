e7b38b95032b3241eb6f0951070e7809a751951e
Mong <mong@channel.io>
2021-04-14 11:54:26 +0900

    arr type에서 index로 값을 들오고자 할 때는 T[number]을 하면 된다는 사실을 알고 계셨나요?

--- questions/11-easy-tuple-to-object/template.ts
+++ questions/11-easy-tuple-to-object/template.ts
@@ -1,1 +1,11 @@
-type TupleToObject<T extends readonly any[]> = any
+type TupleToObject<T extends readonly any[]> = {
+  [K in T[number]]: K
+}
+
+const txampleTuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
+
+const result: TupleToObject<typeof txampleTuple> = {
+  tesla: 'tesla',
+  "model 3": 'model 3',
+  "helki": "helki",
+}
